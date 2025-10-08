a66d437882789278112e47bb0032960621c100f9
MaksTinyWorkshop <makstinyworkshop@gmail.com>
2025-10-08 17:45:51 +0200

    Fix Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,23 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import { webcore } from "webcoreui/integration";
-import node from "@astrojs/node";
 import { rehypeBaseImages } from "./src/lib/rehypeBaseImages";
 
-const siteUrl = "https://urlSite.com";
-const siteBase = "";
+const siteUrl = "https://url.com";
+const siteBase = "/Nom_du_Repo";
 
 export default defineConfig({
   site: siteUrl,
 
   scopedStyleStrategy: "where",
   integrations: [tailwind(), mdx(), webcore()],
-  adapter: node({
-    mode: "standalone",
-  }),
-  output: "static",
+  output: 'static',
   base: siteBase,
   markdown: {
     syntaxHighlight: "prism",
     rehypePlugins: [[rehypeBaseImages, { base: siteBase }]],
   },
+  vite: { base: siteBase },
   alias: {
     "@components": "./src/components",
     "@layouts": "./src/layouts",
